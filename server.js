792619c1382a9a596aec9779b2d1a54b8ab1059d
Colin <clnsdabst@gmail.com>
2015-12-15 21:01:15 -0800

    created very basic framework for app with node express and mongo, will add react later

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express = require('express'),
 		app	= express(),
 		mongoose = require('mongoose'),
 		logger = require('morgan'),
-		bodyParser = require('body-parser')
+		bodyParser = require('body-parser'),
+    ejs = require('ejs'),
+    ejsLayouts = require('express-ejs-layouts')
 
 //Connect to DB
 mongoose.connect('mongodb://localhost/challengr', function(err){
@@ -11,13 +13,25 @@ mongoose.connect('mongodb://localhost/challengr', function(err){
 	console.log('Connected to MongoDB!')
 });
 
+// ejs configuration
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+
 //Set up middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
+// configure our app to handle CORS requests
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, authorization');
+	next();
+});
+
 app.get('/', function(req, res){
-	res.send('Welcome to Challengr')
+	res.render('index')
 });
 
 app.listen(3000, function(){
